f464a98c2744a4b9823345e732b718860c98111d
A113837 <padi196@live.com>
2020-07-17 10:37:57 +0800

    Add files via upload

---
+++ Section 08: Spring Configuration with Java Annotations - Dependency Injection/067. Field Injection - Write Some Code/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,46 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+	
+	@Autowired
+	private FortuneService fortuneService;
+	
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	//define default constructor
+	public TennisCoach() {
+		System.out.println(">>TennisCoach: inside default constructor");
+	}
+
+	
+	  //define a setter method
+	/*
+	 * @Autowired public void doSomeCrazyStuff(FortuneService theFortuneService) {
+	 * fortuneService = theFortuneService;
+	 * System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+	 * 
+	 * }
+	 */
+	 
+	
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice your backhand volley annotation2";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
